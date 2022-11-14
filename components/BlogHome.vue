0300a81628c246f5e164d984677d77deab08f9f7
David Mcneal <davidmcneal28@outlook.com>
2022-11-13 18:34:26 -0700

    fix

--- components/BlogHome.vue
+++ components/BlogHome.vue
@@ -4,10 +4,8 @@
     <ul>
       <li v-for="article of articles" :key="article.slug">
         <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-          <img :src="article.img" />
           <div>
             <h2>{{ article.title }}</h2>
-            <p>by {{ article.author.name }}</p>
             <p>{{ article.description }}</p>
           </div>
         </NuxtLink>
